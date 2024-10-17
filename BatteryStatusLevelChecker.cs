bea54ca1fea4f36132f24693ec6dbeed8cd490b6
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-10-17 22:30:01 +0530

    Update BatteryStatusLevelChecker.cs

--- BatteryStatusLevelChecker.cs
+++ BatteryStatusLevelChecker.cs
@@ -1,1 +1,15 @@
-
+using System;
+using System.Diagnostics;
+namespace paradigm_shift_csharp
+{
+class Checker
+{
+     static int Main() 
+     {
+        ExpectTrue(batteryIsOk(25, 70, 0.7f));
+        ExpectFalse(batteryIsOk(50, 85, 0.0f));
+        Console.WriteLine("All ok");
+        return 0;
+     }    
+}
+}
