86d99eeb08da2a16f6e171164148bbeb2930df68
SowmiyaRaja <34887465+SowmiyaRaja@users.noreply.github.com>
2024-10-17 23:54:41 +0530

    Update BatteryStatusLevelChecker.cs

--- BatteryStatusLevelChecker.cs
+++ BatteryStatusLevelChecker.cs
@@ -6,8 +6,8 @@ public partial class Checker
 {
      static int Main() 
      {
-        ExpectTrue(batteryIsOk(25, 70, 0.7f));
-        ExpectFalse(batteryIsOk(50, 85, 0.0f));
+        ExpectTrue(BatteryIsOk(25, 70, 0.7f));
+        ExpectFalse(BatteryIsOk(50, 85, 0.0f));
         Console.WriteLine("All ok");
         return 0;
      }    
